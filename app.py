4e41e766a5aa571de25d8673054ac9529f8709aa
EmiliaMM <91829290+EmiliaMM@users.noreply.github.com>
2022-05-08 11:08:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+"""kom"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """kom"""   
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
